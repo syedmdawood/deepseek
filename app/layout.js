775e4bed3b9f6097444b9ed1156bc093b96f4124
Syed Muhammad Dawood <syedmuhammaddawood635@gmail.com>
2025-03-22 01:58:26 +0500

    first commit

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
